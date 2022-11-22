105f882665d4ee054b1ae63e155a3e5f4c637836
marlisonmourao <bentesmourao@gmail.com>
2022-11-22 05:37:54 -0400

    setting the app theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
+import { ThemeProvider } from 'styled-components'
+
+import { Button } from './components/Button'
+import { GlobalStyle } from './styles/global'
+
+import { defaultTheme } from './styles/themes/default'
+
 export function App() {
-
   return (
-    <div className="App">
+    <ThemeProvider theme={defaultTheme}>
+      <Button variant="primary" />
+      <Button variant="secondary" />
+      <Button variant="danger" />
 
-    </div>
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
